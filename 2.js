2d312a29a9dfc2365b7bea02b2c44290bd75523b
mosaypup <blyatshotakoe@gmail.com>
2018-11-07 16:07:38 +0200

    sdfsdf

--- 2.js
+++ 2.js
@@ -1,4 +1,4 @@
-var css = 'h1.bg_titel { background: red; }',
+var css = 'img.wp-smiley, img.emoji { display: inline !important; border: none !important; box-shadow: none !important; height: 1em !important; width: 1em !important; margin: 0 .07em !important; vertical-align: -0.1em !important; background: none !important; padding: 0 !important; }.ht_plug_con.nt_mb_class { width: 100%; text-align: center; }video { z-index: 100; } .ht_plug_con.nt_mb_class [class*="wp-image-"],.ht_plug_con.nt_mb_class .ht_img, .ht_img_nt { height: auto; max-width: 100%; } ',
 head = document.head || document.getElementsByTagName('head')[0],
 style = document.createElement('style');
 
@@ -11,3 +11,16 @@ style.appendChild(document.createTextNode(css));
 }
 
 head.appendChild(style);
+
+
+
+.ht_plug_con.nt_mb_class {
+    width: 100%;
+    text-align: center;
+}
+
+
+.ht_plug_con.nt_mb_class [class*="wp-image-"], .ht_plug_con.nt_mb_class .ht_img, .ht_img_nt {
+    height: auto;
+    max-width: 100%;
+}
